5f4bdde1272de636960de68feee0bb92e12c8614
zc00963 <zc00963@ES-TCCT-JXQ93Z2>
2024-05-06 07:20:54 +0200

    Versión inicial

---
+++ GitHubTest/src/edu/medac/git/HolaMundo.java
@@ -0,0 +1,31 @@
+package edu.medac.git;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/* El programa imprime un mensaje 10 veces:
+ * - Iteraciones impares: "Hola mundo"
+ * - Iteraciones pares: "Adiós mundo"
+ */
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		final String HOLA = "Hola Mundo";
+		final String ADIOS = "Adiós Mundo";
+		
+		String imprime; 
+		
+		for (int i=1;i<=10;i++)
+		{
+			if (i%2 == 1)
+				imprime = HOLA;
+			else
+				imprime = ADIOS;
+			
+			System.out.println (i + ": " + imprime);
+		}
+		
+		System.out.println(">>> FIN");
+	}
+}
